ab37f5695bf570e867afbdac5dce4930e97e0a5d
Rupesh Bansal <rupeshbansal@Shankars-MacBook-Air.local>
2023-10-27 12:52:21 +0530

    Added llms

---
+++ infinitesearch/llms/openai.py
@@ -0,0 +1,6 @@
+from base import Base
+
+
+class OpenAi(Base):
+    def __init__(self):
+        pass
--- infinitesearch/utils.py
+++ infinitesearch/utils.py
@@ -1,5 +1,5 @@
 from datatype import DataType
 
+
 def infer_type(source: str) -> DataType:
-
-
+    pass
