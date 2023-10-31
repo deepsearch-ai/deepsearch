1af70b8436f2a7cf51c80519f6755691572f5907
Rupesh Bansal <rupeshbansal@Shankars-MacBook-Air.local>
2023-10-31 21:49:22 +0530

    utils plugin

--- infinitesearch/sources/utils.py
+++ infinitesearch/sources/utils.py
@@ -15,27 +15,9 @@ class SourceUtils:
     def add_data(self, source: str, llm_model: BaseLLM, vector_database: BaseVectorDatabase) -> None:
         datasource = self._infer_type(source)
         if datasource == DataSource.S3:
-            pass
+            self.s3_data_source.add_data(source, llm_model, vector_database)
         elif datasource == DataSource.LOCAL:
-            # Recursively iterate over all the files and subdirectories in the current directory
-            for root, dirs, files in os.walk(source):
-                for file in files:
-                    path = os.path.join(root, file)
-                    try:
-                        data = Image.open(path)
-                    except FileNotFoundError:
-                        print("The supplied file does not exist {}".format(path))
-                        continue
-                    except UnidentifiedImageError:
-                        print("The supplied file is not an image {}".format(path))
-                        continue
-                    except Exception as e:
-                        print("Error while reading file {}".format(path))
-                        print(e)
-                        continue
-
-                    encodings_json = llm_model.get_media_encoding(data)
-                    vector_database.add([encodings_json.get("embedding")], [path], [path])
+            self.local_data_source.add_data(source, llm_model, vector_database)
         else:
             raise ValueError("Invalid data source")
 
