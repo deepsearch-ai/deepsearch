29748e86172c47326b721909b686407e9d2164fc
shiwangi-shah <shiwangishah@google.com>
2023-11-14 11:48:42 +0530

    add theme package for docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_logo = "deepsearch.png"
 html_theme_options = {
