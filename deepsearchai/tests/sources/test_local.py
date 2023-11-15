3eee4aa78c7024770f5146312a54e5b0a884b176
Rupesh Bansal <rupeshbansal@Rupeshs-MacBook-Air.local>
2023-11-15 20:28:37 +0530

    Fixed local data tests

--- deepsearchai/tests/sources/test_local.py
+++ deepsearchai/tests/sources/test_local.py
@@ -6,126 +6,110 @@ import mock.mock
 
 from deepsearchai.enums import MEDIA_TYPE
 from deepsearchai.sources.local import LocalDataSource
+from deepsearchai.sources.data_source import DataSource
 
 
 class LocalDataSourceTest(unittest.TestCase):
     def setUp(self):
         self.local_data_source = LocalDataSource()
 
-    @patch("os.listdir")
+    @patch("os.walk")
     @patch("PIL.Image.open")
     def test_add_data_image_directory_with_no_existing_files(
         self, mock_image_file, mock_listdir
     ):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
+
         # Create a mock image file
-        mock_listdir.return_value = ["image1.jpg", "image2.png"]
-        mock_image_file.return_value = mock.Mock()
+        image_data = mock.Mock()
+        mock_listdir.return_value = [("test_directory", "", ["image1.jpg", "image2.png"])]
+        mock_image_file.return_value = image_data
 
         # Create a mock vector database
         vector_database = mock.Mock()
         vector_database.get_existing_document_ids.return_value = []
 
-        # Create a mock for the arguments to be received from the llm model
-        embeddings = [[0, 0, 0]]
-        metadata = [{"author": "author1"}]
-        ids = ["id1"]
-        encodings_json = {"embedding": embeddings, "metadata": metadata, "ids": ids}
-
-        # Create a mock for the llm model
-        llms_config = mock.Mock()
-        llms_config.get_llm_model.return_value.get_media_encoding.return_value = (
-            encodings_json
-        )
-
         # Add local datasource data for a local directory
         directory = "test_directory"
-        self.local_data_source.add_data(directory, llms_config, vector_database)
+        self.local_data_source.add_data(directory, embedding_models_config, vector_database)
         assert vector_database.add.mock_calls == [
             mock.call(
-                embeddings,
-                ["test_directory/image1.jpg"],
-                ids,
-                metadata,
+                image_data,
+                DataSource.LOCAL,
+                "test_directory/image1.jpg",
+                directory,
                 MEDIA_TYPE.IMAGE,
+                embedding_model
             ),
+
             mock.call(
-                embeddings,
-                ["test_directory/image2.png"],
-                ids,
-                metadata,
+                image_data,
+                DataSource.LOCAL,
+                "test_directory/image2.png",
+                directory,
                 MEDIA_TYPE.IMAGE,
+                embedding_model
             ),
         ]
 
-    @patch("os.listdir")
+    @patch("os.walk")
     @patch("PIL.Image.open")
     def test_add_data_image_directory_with_no_existing_files(
         self, mock_image_file, mock_listdir
     ):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
+
         # Create a mock image file
-        mock_listdir.return_value = ["image1.jpg", "image2.png"]
-        mock_image_file.return_value = mock.Mock()
+        image_data = mock.Mock()
+        mock_listdir.return_value = [("test_directory", "", ["image1.jpg", "image2.png"])]
+        mock_image_file.return_value = image_data
 
         # Create a mock vector database
         vector_database = mock.Mock()
         vector_database.get_existing_document_ids.return_value = []
 
-        # Create a mock for the arguments to be received from the llm model
-        embeddings = [[0, 0, 0]]
-        metadata = [{"author": "author1"}]
-        ids = ["id1"]
-        encodings_json = {"embedding": embeddings, "metadata": metadata, "ids": ids}
-
-        # Create a mock for the llm model
-        llms_config = mock.Mock()
-        llms_config.get_llm_model.return_value.get_media_encoding.return_value = (
-            encodings_json
-        )
-
-        new_metadata_1 = [
-            {
-                "source_type": "LOCAL",
-                "source_id": "test_directory",
-                "document_id": "test_directory/image1.jpg",
-                "author": "author1",
-            }
-        ]
-        new_metadata_2 = [
-            {
-                "source_type": "LOCAL",
-                "source_id": "test_directory",
-                "document_id": "test_directory/image2.png",
-                "author": "author1",
-            }
-        ]
         # Add local datasource data for a local directory
         directory = "test_directory"
-        self.local_data_source.add_data(directory, llms_config, vector_database)
+        self.local_data_source.add_data(directory, embedding_models_config, vector_database)
         assert vector_database.add.mock_calls == [
             mock.call(
-                embeddings,
-                ["test_directory/image1.jpg"],
-                ids,
-                new_metadata_1,
+                image_data,
+                DataSource.LOCAL,
+                "test_directory/image1.jpg",
+                directory,
                 MEDIA_TYPE.IMAGE,
+                embedding_model
             ),
             mock.call(
-                embeddings,
-                ["test_directory/image2.png"],
-                ids,
-                new_metadata_2,
+                image_data,
+                DataSource.LOCAL,
+                "test_directory/image2.png",
+                directory,
                 MEDIA_TYPE.IMAGE,
+                embedding_model
             ),
         ]
 
-    @patch("os.listdir")
+    @patch("os.walk")
     @patch("PIL.Image.open")
     def test_add_data_image_directory_with_existing_files(
         self, mock_image_file, mock_listdir
     ):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
+
         # Create a mock image file
-        mock_listdir.return_value = ["image1.jpg", "image2.png"]
-        mock_image_file.return_value = mock.Mock()
+        image_data = mock.Mock()
+        mock_listdir.return_value = [("test_directory", "", ["image1.jpg", "image2.png"])]
+        mock_image_file.return_value = image_data
 
         # Create a mock vector database, such that one file already exists in it
         vector_database = mock.Mock()
@@ -133,36 +117,17 @@ class LocalDataSourceTest(unittest.TestCase):
             "test_directory/image2.png"
         ]
 
-        # Create a mock for the arguments to be received from the llm model
-        embeddings = [[0, 0, 0]]
-        metadata = [{"author": "author1"}]
-        ids = ["id1"]
-        encodings_json = {"embedding": embeddings, "metadata": metadata, "ids": ids}
-
-        new_metadata = [
-            {
-                "source_type": "LOCAL",
-                "source_id": "test_directory",
-                "document_id": "test_directory/image1.jpg",
-                "author": "author1",
-            }
-        ]
-        # Create a mock for the llm model
-        llms_config = mock.Mock()
-        llms_config.get_llm_model.return_value.get_media_encoding.return_value = (
-            encodings_json
-        )
-
         # Add local datasource data for a local directory
         directory = "test_directory"
-        self.local_data_source.add_data(directory, llms_config, vector_database)
+        self.local_data_source.add_data(directory, embedding_models_config, vector_database)
         assert vector_database.add.mock_calls == [
             mock.call(
-                embeddings,
-                ["test_directory/image1.jpg"],
-                ids,
-                new_metadata,
+                image_data,
+                DataSource.LOCAL,
+                "test_directory/image1.jpg",
+                directory,
                 MEDIA_TYPE.IMAGE,
+                embedding_model
             )
         ]
 
@@ -170,43 +135,34 @@ class LocalDataSourceTest(unittest.TestCase):
     @patch("PIL.Image.open")
     @patch("mimetypes.guess_type")
     def test_add_data_image(self, mock_mimetype, mock_image_file, mock_isfile):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
+
+        image_data = mock.Mock()
         mock_isfile.return_value = True
-        mock_image_file.return_value = mock.Mock()
+        mock_image_file.return_value = image_data
         mock_mimetype.return_value = "image/jpeg", "encoding"
 
         # Create a mock vector database, such that one file already exists in it
         vector_database = mock.Mock()
         vector_database.get_existing_document_ids.return_value = []
-
-        # Create a mock for the arguments to be received from the llm model
-        embeddings = [[0, 0, 0]]
-        metadata = [{"author": "author1"}]
-        ids = ["id1"]
-        encodings_json = {"embedding": embeddings, "metadata": metadata, "ids": ids}
-
         directory = "image1.jpg"
-        # Create a mock for the llm model
-        llms_config = mock.Mock()
-        llms_config.get_llm_model.return_value.get_media_encoding.return_value = (
-            encodings_json
-        )
-        new_metadata = [
-            {
-                "source_type": "LOCAL",
-                "source_id": directory,
-                "document_id": directory,
-                "author": "author1",
-            }
-        ]
+
         # Add local datasource data for a local directory
-        self.local_data_source.add_data(directory, llms_config, vector_database)
+        self.local_data_source.add_data(directory, embedding_models_config, vector_database)
         assert vector_database.add.mock_calls == [
-            mock.call(embeddings, [directory], ids, new_metadata, MEDIA_TYPE.IMAGE)
+            mock.call(image_data, DataSource.LOCAL, directory, directory, MEDIA_TYPE.IMAGE, embedding_model)
         ]
 
     @patch("os.path.isfile")
     @patch("mimetypes.guess_type")
     def test_add_data_audio(self, mock_mimetypes, mock_is_file):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
         # Create a mock image file
         mock_is_file.return_value = True
         mock_mimetypes.return_value = "audio/mp3", "encoding"
@@ -215,35 +171,20 @@ class LocalDataSourceTest(unittest.TestCase):
         vector_database = mock.Mock()
         vector_database.get_existing_document_ids.return_value = []
 
-        # Create a mock for the arguments to be received from the llm model
-        embeddings = [[0, 0, 0]]
-        metadata = [{"author": "author1"}]
-        ids = ["id1"]
-        encodings_json = {"embedding": embeddings, "metadata": metadata, "ids": ids}
-
-        # Create a mock for the llm model
-        llms_config = mock.Mock()
-        llms_config.get_llm_model.return_value.get_media_encoding.return_value = (
-            encodings_json
-        )
         filename = "audio.mp3"
-        new_metadata = [
-            {
-                "source_type": "LOCAL",
-                "source_id": filename,
-                "document_id": filename,
-                "author": "author1",
-            }
-        ]
         # Add local datasource data for a local directory
-        self.local_data_source.add_data(filename, llms_config, vector_database)
+        self.local_data_source.add_data(filename, embedding_models_config, vector_database)
         assert vector_database.add.mock_calls == [
-            mock.call(embeddings, [filename], ids, new_metadata, MEDIA_TYPE.AUDIO)
+            mock.call(filename, DataSource.LOCAL, filename, filename, MEDIA_TYPE.AUDIO, embedding_model)
         ]
 
     @patch("os.path.isfile")
     @patch("mimetypes.guess_type")
     def test_add_data_unsupported_media(self, mock_mimetype, mock_isfile):
+        embedding_models_config = mock.Mock()
+        embedding_model = mock.Mock()
+
+        embedding_models_config.get_embedding_model.return_value = [embedding_model]
         # Create a mock unsupported file
         # Create a mock image file
         mock_isfile.return_value = True
@@ -254,9 +195,7 @@ class LocalDataSourceTest(unittest.TestCase):
         vector_database.get_existing_document_ids.return_value = []
 
         # Create a mock for the llm model
-        llms_config = mock.Mock()
         filename = "random.format"
         # Add local datasource data for a local directory
-        self.local_data_source.add_data(filename, llms_config, vector_database)
-        vector_database.add.assert_not_called
-        llms_config.get_llm_model.assert_not_called
+        self.local_data_source.add_data(filename, embedding_models_config, vector_database)
+        vector_database.add.assert_not_called()
